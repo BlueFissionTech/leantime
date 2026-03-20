0380facbd4311c624b03000b8654bd76de288608
Devon Scott <dscott@bluefission.com>
2026-03-20 15:53:07 -0400

    strengthen support center empty-state styling

--- app/Domain/Supportcenter/Templates/new.blade.php
+++ app/Domain/Supportcenter/Templates/new.blade.php
@@ -5,62 +5,64 @@
 @include('supportcenter::partials.assetStyles')
 
 <div class="maincontent">
-    <div class="maincontentinner">
+    <div class="maincontentinner supportcenter-shell">
         @if(count($supportProjects) === 0)
             <div class="alert alert-info">
                 No support projects are currently available for your account.
             </div>
         @else
-            <form method="post" action="{{ BASE_URL }}/support-center/new" class="form-horizontal supportcenter-editor">
-                <div class="row">
-                    <div class="col-md-8">
-                        <div class="form-group">
-                            <label class="control-label col-md-2" for="projectId">Project</label>
-                            <div class="col-md-10">
-                                <select id="projectId" name="projectId" class="form-control">
-                                    @foreach($supportProjects as $projectOption)
-                                        <option value="{{ $projectOption['id'] }}" @selected(($selectedSupportProject['id'] ?? null) === $projectOption['id'])>
-                                            {{ $projectOption['name'] }}
-                                        </option>
-                                    @endforeach
-                                </select>
+            <div class="supportcenter-panel">
+                <form method="post" action="{{ BASE_URL }}/support-center/new" class="form-horizontal supportcenter-editor">
+                    <div class="row">
+                        <div class="col-md-8">
+                            <div class="form-group">
+                                <label class="control-label col-md-2" for="projectId">Project</label>
+                                <div class="col-md-10">
+                                    <select id="projectId" name="projectId" class="form-control">
+                                        @foreach($supportProjects as $projectOption)
+                                            <option value="{{ $projectOption['id'] }}" @selected(($selectedSupportProject['id'] ?? null) === $projectOption['id'])>
+                                                {{ $projectOption['name'] }}
+                                            </option>
+                                        @endforeach
+                                    </select>
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group">
-                            <label class="control-label col-md-2" for="headline">Subject</label>
-                            <div class="col-md-10">
-                                <input id="headline" type="text" name="headline" value="{{ old('headline') }}" class="form-control" required />
+                            <div class="form-group">
+                                <label class="control-label col-md-2" for="headline">Subject</label>
+                                <div class="col-md-10">
+                                    <input id="headline" type="text" name="headline" value="{{ old('headline') }}" class="form-control" required />
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group">
-                            <label class="control-label col-md-2" for="priority">Priority</label>
-                            <div class="col-md-10">
-                                <select id="priority" name="priority" class="form-control">
-                                    @foreach($priorities as $key => $label)
-                                        <option value="{{ $key }}">{{ $label }}</option>
-                                    @endforeach
-                                </select>
+                            <div class="form-group">
+                                <label class="control-label col-md-2" for="priority">Priority</label>
+                                <div class="col-md-10">
+                                    <select id="priority" name="priority" class="form-control">
+                                        @foreach($priorities as $key => $label)
+                                            <option value="{{ $key }}">{{ $label }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group">
-                            <label class="control-label col-md-2" for="description">Description</label>
-                            <div class="col-md-10">
-                                <textarea id="description" name="description" rows="10" class="form-control tiptapComplex" required>{{ old('description') }}</textarea>
+                            <div class="form-group">
+                                <label class="control-label col-md-2" for="description">Description</label>
+                                <div class="col-md-10">
+                                    <textarea id="description" name="description" rows="10" class="form-control tiptapComplex" required>{{ old('description') }}</textarea>
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group">
-                            <div class="col-md-offset-2 col-md-10">
-                                <button type="submit" class="btn btn-primary">Create Ticket</button>
-                                <a class="btn btn-default" href="{{ BASE_URL }}/support-center?projectId={{ $selectedSupportProject['id'] ?? '' }}">Cancel</a>
+                            <div class="form-group">
+                                <div class="col-md-offset-2 col-md-10">
+                                    <button type="submit" class="btn btn-primary">Create Ticket</button>
+                                    <a class="btn btn-default" href="{{ BASE_URL }}/support-center?projectId={{ $selectedSupportProject['id'] ?? '' }}">Cancel</a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </form>
+                </form>
+            </div>
         @endif
     </div>
 </div>
